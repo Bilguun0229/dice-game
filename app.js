3b8a538b32f00486a9d8c955fd641035b5d69fc9
pegu <pegu.mgl@gmail.com>
2022-03-24 16:47:28 +0800

    shoog shiddeg bolgoh

---
+++ app.js
@@ -0,0 +1,20 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийн 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
+var activePlayer = 1;
+// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+// Шооны аль нэг талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var diceNumber=Math.floor(Math.random() * 6)+1;
+// Програм эхлэхэд бэлдэе
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-'+diceNumber + ".png"
+}) ;
